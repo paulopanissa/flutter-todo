025aea7171912030b7f8b87487bcc07e8a42a76a
PauLo Panissa <paulopanissa@pop-os.localdomain>
2020-02-10 21:25:46 -0400

    feat - aprendendo flutter: <todo list>

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Todo App"),
+        ),
+        body: Container(
+          child: Center(
+            child: Text("Olá Mundo"),
+          ),
+        ));
+  }
+}
